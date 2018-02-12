dc3da5b8a1515b057fef65eae6b00fa68d5d13f2
Nikola Mihalek <nikola.mihalek@gmail.com>
2018-02-12 04:22:23 +0100

    added milestone/version crud. added forms. added buttons for crud functionality. changed existing templates to use bootstrap4. made pages look nicer.  added tests for new forms. tweaked docker-compose for database wait and .travis to be more detailed with tests

--- app/project/models.py
+++ app/project/models.py
@@ -5,15 +5,15 @@ from .managers import ProjectManager
 
 
 def default_user():
-        try:
-            return User.objects.get(username='GhostUser')
-        except :
-            return User.objects.create_user(username='GhostUser', email='ghost.user@ukshub.com', password='some password')
+    try:
+        return User.objects.get(username='GhostUser')
+    except :
+        return User.objects.create_user(username='GhostUser', email='ghost.user@ukshub.com', password='some password')
 
 
 class Project(models.Model):
 
-    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, default=default_user, related_name="projects")
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, default=default_user, related_name="projects", blank=False)
     name = models.CharField(max_length=100)
     description = models.TextField()
     date_created = models.DateField('Date created',auto_now_add=True)
@@ -27,27 +27,32 @@ class Project(models.Model):
         return reverse('project:project_detail', kwargs={'pk': self.pk})
 
     def __str__(self):
-        return self.name + ' (' + self.owner.username + ')'
+        return "%s (%s)" % (self.name, self.owner)
 
 class Milestone(models.Model):
-    project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE, related_name='milestones', blank=False)
+    name = models.CharField(max_length=100)
     due_date = models.DateField('Due date')
     description = models.TextField()
+
+    class Meta:
+        unique_together = (("project", "name"),)
 
     def get_absolute_url(self):
         return reverse('project:milestone_detail', kwargs={'pk': self.pk})
 
     def __str__(self):
-        return "[%s]:" % self.project.name + self.title
-
+        return "Milestone: %s (%s)" % (self.name, self.project.name)
 
 class Version(models.Model):
-    project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE, related_name='versions', blank=False)
     name = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together = (("project", "name"),)
 
     def get_absolute_url(self):
         return reverse('project:version_detail', kwargs={'pk': self.pk})
     
     def __str__(self):
-        return "[%s]:" % self.project.name + self.name
+        return "Version: %s (%s)" % (self.name, self.project.name)
