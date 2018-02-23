45412bd273222fe68871978f708d0a5fd02ca7b0
Nikola Mihalek <nikola.mihalek@gmail.com>
2018-02-23 04:21:10 +0100

    added member management on projects

--- app/project/forms.py
+++ app/project/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Field
 from .models import Project, Milestone, Version
@@ -10,7 +11,7 @@ class ProjectForm(forms.ModelForm):
         super(ProjectForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_id = 'id-projectForm'
-        self.helper.form_class = 'd-table mx-auto justify-content-center'
+        self.helper.form_class = 'd-table mx-auto justify-content-sm-center'
         self.helper.label_class = 'font-weight-bold'
         self.helper.field_class = 'form-control-sm'
         self.helper.form_group_wrapper_class = 'w-100'
@@ -22,6 +23,31 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['name', 'description',]
+
+class AddMemberForm(forms.Form):
+
+    username = forms.SlugField(max_length=150)
+
+    def __init__(self, *args, **kwargs):
+        super(AddMemberForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-addMemberForm'
+        self.helper.form_class = 'd-table mx-auto justify-content-sm-center'
+        self.helper.label_class = 'font-weight-bold'
+        self.helper.field_class = 'form-control-sm'
+        self.helper.form_group_wrapper_class = 'w-100'
+        self.helper.form_method = 'post'
+        self.helper.form_action = ''
+
+        self.helper.add_input(Submit('submit', 'Submit'))
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            return username
+        else:
+            raise forms.ValidationError('User: %s does not exist.' % username)
+
     
 class MilestoneForm(forms.ModelForm):
 
@@ -29,7 +55,7 @@ class MilestoneForm(forms.ModelForm):
         super(MilestoneForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_id = 'id-milestoneForm'
-        self.helper.form_class = 'd-table mx-auto justify-content-center'
+        self.helper.form_class = 'd-table mx-auto justify-content-sm-center'
         self.helper.label_class = 'font-weight-bold'
         self.helper.field_class = 'form-control-sm'
         self.helper.form_group_wrapper_class = 'w-100'
@@ -51,7 +77,7 @@ class VersionForm(forms.ModelForm):
         super(VersionForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_id = 'id-versionForm'
-        self.helper.form_class = 'd-table mx-auto justify-content-center'
+        self.helper.form_class = 'd-table mx-auto justify-content-sm-center'
         self.helper.label_class = 'font-weight-bold'
         self.helper.field_class = 'form-control-sm'
         self.helper.form_group_wrapper_class = 'w-100'
--- app/project/views.py
+++ app/project/views.py
@@ -1,14 +1,15 @@
+from django import forms
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, AnonymousUser
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView, ModelFormMixin
+from django.views.generic.detail import DetailView, SingleObjectMixin
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from guardian.mixins import PermissionRequiredMixin
 from guardian.shortcuts import assign_perm
 from .models import Project, Version, Milestone
-from .forms import ProjectForm, VersionForm, MilestoneForm
+from .forms import ProjectForm, AddMemberForm, VersionForm, MilestoneForm
 
 class HomePageView(ListView):
 
@@ -62,6 +63,50 @@ class ProjectDetailView(DetailView):
     model = Project
     context_object_name = 'project'
 
+
+class ProjectMemberAddView(FormView, SingleObjectMixin, PermissionRequiredMixin):
+    form_class = AddMemberForm
+    model = Project
+    template_name = 'project/project_member_add_form.html'
+    context_object_name = 'project'
+    permission_required = 'change_project'
+
+    def form_valid(self, form):
+        member_username = form.cleaned_data['username']
+        member = User.objects.get(username=member_username)
+        self.object.members.add(member)
+        self.object.save()
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        project_id = self.kwargs['pk']
+        return reverse_lazy('project:project_detail', args=[project_id])
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().get(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+
+class ProjectMemberDeleteView(DeleteView, PermissionRequiredMixin):
+    model = Project
+    template_name = 'project/project_member_confirm_remove.html'
+
+    def delete(self, request, *args, **kwargs):
+        project = self.get_object()
+        project.members.remove(self.get_user_object(pk=self.kwargs['upk']))
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        project_id = self.kwargs['pk']
+        return reverse_lazy('project:project_detail', args=[project_id])
+    
+    def get_user_object(self, pk):
+        return User.objects.get(pk=pk)
+
 class MilestoneCreateView(LoginRequiredMixin, CreateView):
     model = Milestone
     form_class = MilestoneForm
@@ -71,7 +116,6 @@ class MilestoneCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         project_id = self.kwargs['pk']
         form.instance.project = Project.objects.get(id=project_id)
-        print(form.instance.project)
         return_value = super().form_valid(form)
         assign_perm("delete_milestone", self.request.user, form.instance)
         assign_perm("change_milestone", self.request.user, form.instance)
@@ -90,7 +134,7 @@ class MilestoneDeleteView(PermissionRequiredMixin, DeleteView):
     permission_required = 'delete_milestone'
 
     def get_success_url(self):
-        return reverse_lazy('project:project_detail', kwargs={'pk':self.get_object().project.id})
+        return reverse_lazy('project:project_detail', kwargs={'pk': self.get_object().project.id})
 
 class MilestoneListView(ListView):
     model = Milestone
@@ -110,7 +154,6 @@ class VersionCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         project_id = self.kwargs['pk']
         form.instance.project = Project.objects.get(id=project_id)
-        print(form.instance.project)
         return_value = super().form_valid(form)
         assign_perm("delete_version", self.request.user, form.instance)
         assign_perm("change_version", self.request.user, form.instance)
@@ -129,7 +172,7 @@ class VersionDeleteView(PermissionRequiredMixin, DeleteView):
     permission_required = 'delete_version'
 
     def get_success_url(self):
-        return reverse_lazy('project:project_detail', kwargs={'pk':self.get_object().project.id})
+        return reverse_lazy('project:project_detail', kwargs={'pk': self.get_object().project.id})
 
 class VersionListView(ListView):
     model = Version
